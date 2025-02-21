36e5eb5934409691a096f982377cf466cc39aa40
Юлия Маркина <tiffanimar005@gmail.com>
2025-02-21 12:41:32 +0300

    class Byuer

--- task1/Shops/Shops/Buyer.cpp
+++ task1/Shops/Shops/Buyer.cpp
@@ -1,11 +1,40 @@
 #include "Product.h"
+using namespace std;
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+
+class ProductInfo {
+public:
+	map<string, unsigned>productList;
+	ProductInfo() {
+		productList["cake"] = 30;
+		productList["milk"] = 80;
+		productList["apple"] = 60;
+		productList["yougurt"] = 70;
+		productList["banana"] = 75;
+		productList["bread"] = 65;
+		productList["candes"] = 50;
+		productList["cheese"] = 90;
+
+	}
+	int quantity() {
+		return productList.size();
+	}
+};
+
+
 class Buyer {
+public:
 	string name;
 	int id;
 	int balance;
-	map<Product, int>productList;
+	string pr;
+	int finalprice = 0;
+	map<string, unsigned>productList;
 
-	Buyer() {
+	void Buy() {
 		cout << "Enter your name: ";
 		cin >> name;
 
@@ -14,6 +43,35 @@ class Buyer {
 
 		cout << "Enter your balance: ";
 		cin >> balance;
-			
+
+		productList = productInfo.productList;
+
+		while (true) {
+			cout << "select products from the list (or 'break' to finish): " << endl;
+			cin >> pr;
+
+			if (pr == "break") {
+				break;
+			}
+
+			if (productList.count(pr)) {
+				finalprice += productList[pr];
+				cout << "Summ: " << finalprice << endl;
+			}
+			else {
+				cout << "Sorry, your product is not listed" << endl;
+			}
+		}
 	}
 };
+
+int main() {
+	ProductInfo productInfo;
+	cout << "Количество элементов в map: " << productInfo.quantity() << endl;
+
+
+	Buyer buyerr;
+	buyerr.Buy();
+
+	return 0;
+}
